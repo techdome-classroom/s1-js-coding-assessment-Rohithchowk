702d9930cc6821654a4adf9d796cb10604fce588
Rohithhowk <rohith28chowki@gmail.com>
2024-11-11 13:28:48 +0530

    solved the program2

--- program2.js
+++ program2.js
@@ -39,31 +39,3 @@ const decodeTheRing = function (s, p) {
 
 module.exports = decodeTheRing;
 
-
-//I have also solved above in DP
-
-// const decodeTheRing = function (s, p) {
-//   const dp = Array(s.length + 1).fill(null).map(() => Array(p.length + 1).fill(false));
-
-//   dp[0][0] = true;
-
-//   for (let j = 1; j <= p.length; j++) {
-//     if (p[j - 1] === '*') {
-//       dp[0][j] = dp[0][j - 1];
-//     }
-//   }
-
-//   for (let i = 1; i <= s.length; i++) {
-//     for (let j = 1; j <= p.length; j++) {
-//       if (p[j - 1] === s[i - 1] || p[j - 1] === '?') {
-//         dp[i][j] = dp[i - 1][j - 1];
-//       } else if (p[j - 1] === '*') {
-//         dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-//       }
-//     }
-//   }
-
-//   return dp[s.length][p.length];
-// };
-
-// module.exports = decodeTheRing;
